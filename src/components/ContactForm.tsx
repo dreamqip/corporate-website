161ee67d525af1f1b335453cf78f23eec4e4129b
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-07-15 15:04:15 +0200

    fix(form): update mobile design

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -38,27 +38,24 @@ export default function ContactForm() {
 
   return (
     <form ref={ref} onSubmit={handleSubmit}>
-      <div className='flex border-y-2 border-accent-2'>
-        <label className='flex w-36 items-center py-8 sm:w-48' htmlFor='name'>
-          Name
-        </label>
+      <div className='flex min-h-[9rem] flex-col-reverse border-y-2 border-accent-2 sm:flex-row-reverse'>
         <input
-          className='flex flex-1 items-center justify-center self-stretch border-0 bg-background  placeholder:placeholder-opacity-100 focus:outline-none sm:placeholder:placeholder-opacity-0'
+          className='peer/name flex flex-1 items-center justify-center self-stretch border-0 bg-background  text-sm focus:-mb-[2px] focus:border-b-2 focus:outline-none'
           type='text'
           name='name'
           id='name'
           required
         />
-      </div>
-      <div className='flex border-b-2 border-accent-2'>
         <label
-          className='hidden w-36 items-center py-8 sm:flex sm:w-48'
-          htmlFor='phone'
+          className='flex w-36 items-center pt-8 text-accent-5 transition-colors peer-focus/name:text-foreground sm:w-48 sm:py-8'
+          htmlFor='name'
         >
-          Phone
+          Name
         </label>
+      </div>
+      <div className='flex min-h-[9rem] flex-col-reverse border-b-2 border-accent-2 sm:flex-row-reverse'>
         <input
-          className='flex flex-1 items-center justify-center self-stretch border-0 bg-background placeholder:placeholder-opacity-100 invalid:border-b-2 invalid:border-error invalid:ring-error focus:outline-none sm:placeholder:placeholder-opacity-0'
+          className='peer/phone flex flex-1 items-center justify-center self-stretch border-0 bg-background text-sm focus:-mb-[2px] focus:border-b-2 focus:outline-none'
           type='tel'
           name='phone'
           id='phone'
@@ -66,16 +63,16 @@ export default function ContactForm() {
           pattern='[0-9\-\+\(\) ]+'
           title='Only numbers, spaces and these symbols are allowed: ( ) + - .'
         />
-      </div>
-      <div className='group flex border-b-2 border-accent-2'>
         <label
-          className='hidden w-36 items-center py-8 sm:flex sm:w-48'
-          htmlFor='email'
+          className='flex w-36 items-center pt-8 text-accent-5 transition-colors peer-focus/phone:text-foreground sm:w-48 sm:py-8'
+          htmlFor='phone'
         >
-          Email
+          Phone
         </label>
+      </div>
+      <div className='flex min-h-[9rem] flex-col-reverse border-b-2 border-accent-2 sm:flex-row-reverse'>
         <input
-          className='flex flex-1 items-center justify-center self-stretch border-0 bg-background placeholder:placeholder-opacity-100 invalid:border-error invalid:ring-error focus:outline-none sm:placeholder:placeholder-opacity-0'
+          className='peer/email flex flex-1 items-center justify-center self-stretch border-0 bg-background text-sm focus:-mb-[2px] focus:border-b-2 focus:outline-none'
           type='email'
           name='email'
           id='email'
@@ -83,24 +80,30 @@ export default function ContactForm() {
           pattern='[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$'
           title='Email (Format: example@test.com)'
         />
-      </div>
-      <div className='flex border-b-2 border-accent-2'>
         <label
-          className='sr-only flex w-36 items-center py-8 sm:not-sr-only sm:w-48'
+          className='flex w-36 items-center pt-8 text-accent-5 transition-colors peer-focus/email:text-foreground sm:w-48 sm:py-8'
+          htmlFor='email'
+        >
+          Email
+        </label>
+      </div>
+      <div className='flex min-h-[9rem] flex-col-reverse border-b-2 border-accent-2 sm:flex-row-reverse'>
+        <textarea
+          className='peer/message flex flex-1 resize-none items-center justify-center bg-background text-sm leading-6 placeholder:opacity-0 focus:-mb-[2px] focus:border-b-2 focus:outline-none pt-4 sm:pt-8 sm:placeholder:opacity-0'
+          name='message'
+          id='message'
+          rows={5}
+          required
+        />
+        <label
+          className='flex w-36 items-start pt-8 text-accent-5 transition-colors peer-focus/message:text-foreground sm:w-48 sm:py-8'
           htmlFor='message'
         >
           How can we help?
         </label>
-        <textarea
-          className='flex flex-1 resize-none items-center justify-center self-stretch border-0 bg-background placeholder:opacity-0 focus:outline-none sm:placeholder:opacity-0'
-          name='message'
-          id='message'
-          placeholder='How can we help?'
-          required
-        />
       </div>
       <button
-        className='mt-16 w-36 cursor-pointer p-4 font-medium shadow-[0_0_0_2px] transition-shadow hover:shadow-[0_0_0_4px]'
+        className='mt-16 w-full sm:w-36 cursor-pointer p-4 font-medium shadow-[0_0_0_2px] transition-shadow hover:shadow-[0_0_0_4px]'
         type='submit'
       >
         Send
