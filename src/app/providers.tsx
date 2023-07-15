472591c2c32466b7af8f36d360c22e7284f6cb78
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-07-15 10:49:14 +0200

    feat: add Vercel Analytics integration

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,14 @@
 
 import type { ReactNode } from 'react';
 import { Toaster } from 'sonner';
-// import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <>
       <Toaster theme='dark' closeButton richColors />
       {children}
-      {/*<Analytics />*/}
+      <Analytics />
     </>
   );
 }
