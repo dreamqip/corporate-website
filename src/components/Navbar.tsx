bb131486ddb34a0cf1fb0345223de437038c1f22
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-07-15 15:29:53 +0200

    fix(navbar): correct mobile navbar colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,10 +10,6 @@ const navigation = [
   { name: 'Collaboration', href: '#collaboration', current: false },
 ];
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
-}
-
 export default function Navbar() {
   return (
     <Disclosure
@@ -26,7 +22,7 @@ export default function Navbar() {
             <div className='relative flex h-24 items-center justify-between sm:h-32'>
               <div className='absolute inset-y-0 right-0 flex items-center sm:hidden'>
                 {/* Mobile menu button*/}
-                <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-accent-6 hover:bg-accent-2/50 hover:text-foreground focus:outline-none focus:ring-2 focus:ring-inset focus:ring-foreground'>
                   <span className='sr-only'>Open main menu</span>
                   {open ? (
                     <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
@@ -64,18 +60,13 @@ export default function Navbar() {
           </div>
 
           <Disclosure.Panel className='sm:hidden'>
-            <div className='space-y-1 px-2 pb-3 pt-2'>
+            <div className='grid space-y-1 px-2 pb-3 pt-2'>
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
                   as='a'
                   href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium',
-                  )}
+                  className='rounded-md p-2 font-medium transition-colors duration-200 hover:bg-accent-2/40 hover:text-foreground'
                   aria-current={item.current ? 'page' : undefined}
                 >
                   {item.name}
