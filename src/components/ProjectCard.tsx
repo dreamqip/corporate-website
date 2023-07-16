e84ba9f4d44fd44f6589d4a146e4c8284e30da78
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-07-16 15:58:01 +0200

    fix(project-card): add aspect-ration to the image

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,7 +3,13 @@ import { Project } from '@/types';
 
 type Props = Project;
 
-export default function Card({ name, date, tags, image, description }: Props) {
+export default function ProjectCard({
+  name,
+  date,
+  tags,
+  image,
+  description,
+}: Props) {
   const formatter = new Intl.DateTimeFormat('en-US', {
     year: 'numeric',
     month: 'short',
@@ -13,16 +19,14 @@ export default function Card({ name, date, tags, image, description }: Props) {
   return (
     <>
       <div className='group relative'>
-        <div className='relative overflow-hidden'>
-          <Image
-            className='w-full overflow-hidden border-none grayscale transition-all duration-300 group-hover:grayscale-0'
-            src={image}
-            alt={name}
-            width={400}
-            height={432}
-          />
-          <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent transition-all duration-300' />
-        </div>
+        <Image
+          className='aspect-[3/2] w-full overflow-hidden border-none object-cover outline-none grayscale transition-all duration-300 group-hover:grayscale-0'
+          src={image}
+          alt={name}
+          width={400}
+          height={432}
+        />
+        <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent transition-all duration-300' />
         <div className='absolute bottom-0 left-8 z-10 flex flex-col items-start'>
           <div className='flex items-center'>
             <h4 className='text-accent-6'>
@@ -31,7 +35,7 @@ export default function Card({ name, date, tags, image, description }: Props) {
             {tags.map((tag) => (
               <span
                 key={tag}
-                className='ml-3 text-sm rounded-2xl bg-[rgba(247,_247,_247,_0.10)] px-2 py-1 text-accent-5 backdrop-blur-md first-of-type:ml-3'
+                className='ml-3 rounded-2xl bg-[rgba(247,_247,_247,_0.10)] px-2 py-1 text-sm text-accent-5 backdrop-blur-md first-of-type:ml-3'
               >
                 {tag}
               </span>
